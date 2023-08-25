09b7dd8db6c68687f722f2540b8f11d139c0e3c3
Aareon Sullivan <askully13@gmail.com>
2023-08-25 12:56:34 -0500

    Add example config, remove config.py

--- example_config.py
+++ example_config.py
@@ -1,8 +1,8 @@
-print("Config loaded")
 # Create dummy secret key so we can use sessions
 SECRET_KEY = "123456790"
 
 # Create in-memory database
 DATABASE_FILE = "jobwarden.sqlite"
 
+# Token to be used by employees on the registration page
 ACCESS_TOKEN = "apple"
